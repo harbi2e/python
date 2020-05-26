0f8fd8d372a2d3bd6e623142595d9f5aa9a373b7
harbi2e <harbi2e@gmail.com>
2020-05-26 22:13:29 +0300

    String and Replace

--- My New App.py
+++ My New App.py
@@ -5,3 +5,15 @@ print ("Hello World!")
 print ('''This new multi line hello world
 Today start learn Python lunguage with Fadi Alaswadi
 Thinks Mr.Fadi''')
+
+#string learn
+fname = 'Eyad'
+lname = 'Almohammadi'
+print (fname.lower() + " " + lname.upper())
+
+
+#How to replace any string
+firstname = 'Eyad'
+lastname = 'Almohammadi'
+full_name = 'Eyad Almohammadi'
+print (full_name.replace('Almohammadi','Mohammed'))
