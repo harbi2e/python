3ab47eaca6eeb65a5e167638a4e3f73f7e26b3b7
harbi2e <harbi2e@gmail.com>
2020-05-26 14:38:36 +0300

    New App and multi line

---
+++ My New App.py
@@ -0,0 +1,7 @@
+#This is My New Application
+print('Welcome to My New App \n')
+print ("Hello World!")
+#Multi line
+print ('''This new multi line hello world
+Today start learn Python lunguage with Fadi Alaswadi
+Thinks Mr.Fadi''')
