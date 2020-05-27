e666199af5073720429f9756d42fb180d5b54017
harbi2e <harbi2e@gmail.com>
2020-05-27 13:54:16 +0300

    update True and False commit

---
+++ TrueFalse.py
@@ -0,0 +1,15 @@
+print (True and True)#True
+print (True and False)#False
+print (False and False)#False
+
+print (True or True)#True
+print (True or False)#True
+print (False or False)#False
+
+print (not True)#False
+print (not False)#True
+
+# < , > , == , != , <= , >= المعاملات المنطقية
+a,b = 2,5
+print (b)#5
+print (a)#2
