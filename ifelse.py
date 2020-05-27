ca02b2b870e1a88971b8abc9bfbe0ea1ad492076
harbi2e <harbi2e@gmail.com>
2020-05-27 14:50:44 +0300

    ifelse commit

---
+++ ifelse.py
@@ -0,0 +1,6 @@
+username = input('Please insert your username: ')
+password = input('Please insert your password: ')
+if username == 'Eyad' and password == '102030':
+    print ('Welcome to your CPanel')
+else:
+    print ("Error")
