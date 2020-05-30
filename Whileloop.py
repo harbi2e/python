282ade1498cb2a5ae456bcf9e6ff492eb6948e24
harbi2e <harbi2e@gmail.com>
2020-05-30 19:59:23 +0300

    commit whileloop

--- Whileloop.py
+++ Whileloop.py
@@ -10,3 +10,16 @@ while num_of_tries != 3:
         print('Error')
         num_of_tries +=1
         print('You have ' + str(3 - num_of_tries) + " tries ot left")
+
+num_of_tries = 0
+while num_of_tries != 3:
+    username = input('please insert your username: ')
+    password = input('please insert your password: ')
+    if username == 'Eyad' and password == '123456':
+        print('Welcome')
+        num_of_tries = 0
+        exit()
+    else:
+        print('Error')
+        num_of_tries +=1
+        print(' you have ' + str(3 - num_of_tries) + " trise to lift")
