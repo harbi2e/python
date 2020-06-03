30eff5e2f2bd133636465b96753c41df8d5dac71
harbi2e <harbi2e@gmail.com>
2020-06-03 22:23:17 +0300

    commit scan

--- PythoHack/scannerport.py
+++ PythoHack/scannerport.py
@@ -1,12 +1,15 @@
 import socket
 
 host = input("please enter the ip address: ")
+#hostIP = socket.gethostbyname(host) تحويل رابط الموقع الى اي بي
 try:
     for port in range(1,1000) :
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((host,port))
         if result == 0:
-            print("the port {} is open.".format(port))
+            print("[+] Port {}: Open.".format(port))
+        else:
+            print("[-]" Port {}: Closed.".format(port))
 except:
-    print("Error")
+    print("Pass")
